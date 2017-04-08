97418bc58c71c834296813512d736fd82f448b2a
piropaolo <piropaolo27@gmail.com>
2017-04-08 18:00:34 +0200

    Add new comments, debug

--- elipsa.asm
+++ elipsa.asm
@@ -1,7 +1,7 @@
 .data
-	prompt: .asciiz "Enter major axis: "
+	prompt:	.asciiz "Enter major axis: "
 	prompt2:	.asciiz	"Enter minor axis: "
-	nazwa_pliku: .asciiz "Ellipse.bmp"
+	filename: .asciiz "Ellipse.bmp"
 	.align 2
 	header: .space 56
 .text
@@ -9,11 +9,11 @@
 #$s0 - image width
 #$s1 - image height
 #$s2 - image width with excess
-#$s3 - image buffer adress
-#$s4 - file beginning adress
+#$s3 - image buffer address
+#$s4 - file beginning address
 #$s5 - image size
-#$s6 - semi-major axis
-#$s7 - semi-minor axis
+#$s6 - semi-major axis - a
+#$s7 - semi-minor axis - b
 
 #Output 1st prompt
 	li $v0, 4
@@ -65,43 +65,53 @@
 
 #Allocating memory for the image and header write
 	mul $t1, $s2, $s1	#Pixels needed for the whole pic
-	add $s5, $t1, $zero	#Remember for later
+	add $s5, $t1, $zero	#Remember for later - size
 
 	li $v0, 9		#Allocate memory (nr of bytes)
 	add $a0, $t1, $zero
 	syscall
-	add $s3, $v0, $zero	#Remember the adress to the allocated memory
+	add $s3, $v0, $zero	#Remember the address to the allocated memory
 
-	addi $t1, $t1, 54	#Rozmiar ca�ego pliku czyli rozmiar obrazka i naglowek w $t1
-	sw $t1, 4($t0)		#Wpisz rozmiar ca�ego pliku do nag��wka
-
-	li $t1, 54		#54 - offset danych (rozmiar nag��wka)
+	addi $t1, $t1, 54	#Image size + header
+	sw $t1, 4($t0)		#Size to the header
+	li $t1, 54		#Offset
 	sw $t1, 12($t0)
-	li $t1, 40		#40 - d�ugo�� do ko�ca nag��wka
+	li $t1, 40		#Till the end of header
 	sw $t1, 16($t0)
-	add $t1, $s0, $zero	#Skopiuj szeroko��/wysoko�� obrazka
-	sw $t1, 20($t0)		#szeroko��
-	add $t1, $s1, $zero
-	sw $t1, 24($t0)		#wysoko��
-	li $t1, 1		#1 - ilo�� wartstw kolor�w
+	add $t1, $s0, $zero	#Copying image width
+	sw $t1, 20($t0)
+	add $t1, $s1, $zero	#Copying image height
+	sw $t1, 24($t0)
+	li $t1, 1		#The number of color planes
 	sw $t1, 28($t0)
-	li $t1, 24		#24 - liczba bit�w na piksel (3 kolory po 8 bit�w ka�dy)
+	li $t1, 24		#The number of bits per pixel
 	sb $t1, 30($t0)
-#Otw�rz plik do zapisu
-	li $v0, 13		#Otwieranie pliku
-	la $a0, nazwa_pliku	#Wczytanie adresu na nazw� p	liku
-	li $a1, 1		#1 - bo plik do odczytu
-	li $a2, 0		#0 - flaga
+
+#Open up file to write
+	li $v0, 13		#Open file syscall
+	la $a0, filename
+	li $a1, 1		#1 - write-only with create
+	li $a2, 0		#0 - flag
 	syscall
-	add $s4, $v0, $zero	#Zapami�tanie wska�nika na plik w $s3
-#Zapisz nag��wek
-	li $v0, 15
-	add $a0, $s4, $zero	#Skopowianie wska�nika na plik
-	la $a1, header+2	#Pocz�tek zapisywanych danych
-	li $a2, 54		#Ilo�� bit�w do zapisania (rozmiar nag��wka)
-	syscall
+	add $s4, $v0, $zero	#Image pointer adress
+
+#Save header
+	li $v0, 15	#Write to file syscall
+	add $a0, $s4, $zero	#File descriptor
+	la $a1, header+2	#Input buffer
+	li $a2, 54		#Number of characters to write
+	syscall	#GIT GIT
 
 #Bresenham's algorithm
+#$t0 - x0
+#$t1 - y0
+#$t2 - x
+#$t3 - y
+#$t5 - limit
+#$t6 - d
+#$t7 - delta_A
+#$t8 - delta_B
+
 	move $t0, $s6	#x0
 	move $t1, $s7 #y0
 
@@ -120,17 +130,17 @@
 	sll $t4, $t4, 1	#8 * b * a2
 	sub $t8, $t7, $t4
 	add $t8, $t8, $t2
-	add $t8, $t8, $t2	#delta_B
+	add $t8, $t8, $t2	#delta_B = 4 * (3 * b2 - 2 * b * a2 + 2 * a2)
 
-	add $t3, $t3, $t2
-	mul $t2, $t2, $t2
+	add $t3, $t3, $t2	#a2 + b2
+	mul $t2, $t2, $t2	#a2 * a2
 
-	div $t5, $t2, $t3
+	div $t5, $t2, $t3	#limit = (a2 * a2 / a2 + b2)
 
-	move $t2, $zero
-	move $t3, $s7
+	move $t2, $zero	#x = 0
+	move $t3, $s7	#y = b
 
-petla:
+loop:
 #Ustawianie kolor�w pikseli:
 #Ustaw kolor piksela 1
 sub $a0, $t0, $t2	# x0 - x
@@ -185,50 +195,54 @@ sb $v0, ($a0)		#Kolor niebieski
 sb $v0, 1($a0)		#Kolor zielony
 sb $v0, 2($a0)		#Kolor czerwony  (255,255,255)
 
-mul $t4, $t2, $t2
-bge $t4, $t5, koniec
+#Bresenham's algorithm continue
+	mul $t4, $t2, $t2	#x * x
+	bge $t4, $t5, end	#if x * x >= limit
 
-bltz $t6, d0
+	ble $t6, $zero
 
-	add $t6, $t6, $t8
+	add $t6, $t6, $t8	#d += delta_B
 
-	mul $a0, $s6, $s6
-	sll $a0, $a0, 3
+	mul $a0, $s6, $s6	#a2 = a * a
+	sll $a0, $a0, 3	#8 * a2
 
-	mul $a1, $s7, $s7
-	sll $a1, $a1, 3
+	mul $a1, $s7, $s7	#b2 = b * b
+	sll $a1, $a1, 3	#8 * b2
 
-	add $t7, $t7, $a1
-	add $t8, $t8, $a0
+	add $t7, $t7, $a1	#delta_A += 4 * 2 * b2
+	add $t8, $t8, $a0	#delta_B += 4 * (2 * b2 + 2 * a2)
 	add $t8, $t8, $a1
 
-	addi $t2, $t2, 1
-	addi $t3, $t3, -1
-	b dalej
+	addi $t2, $t2, 1	#x += 1
+	addi $t3, $t3, -1	#y -= 1
+	b next
+
 d0:
-	add $t6, $t6, $t7
+	add $t6, $t6, $t7 #d += delta_A
 
-	mul $a1, $s7, $s7
-	sll $a1, $a1, 3
+	mul $a1, $s7, $s7 #b2 = b * b
+	sll $a1, $a1, 3	#8 * b2
 
-	add $t7, $t7, $a1
-	add $t8, $t8, $a1
-	addi $t2, $t2, 1
+	add $t7, $t7, $a1	#delta_A += 4 * 2 * b2
+	add $t8, $t8, $a1	#delta_B += 4 * 2 * b2
+	addi $t2, $t2, 1	#x += 1
 
-dalej:
-	b petla
+next:
+	b loop
 
-koniec:
-#Zapisz reszt� pliku
-	li $v0, 15		#zapis
-	add $a0, $s4, $zero	#Skopiowanie wska�nika na plik
-	add $a1, $s3, $zero	#Skopiowanie adresu bufora
-	add $a2, $s5, $zero	#Skopiowanie ilo�ci pikseli obrazka
+end: #JEST GIT
+#Save the rest of the file
+	li $v0, 15	#Write to file syscall
+	add $a0, $s4, $zero	#File descriptor
+	add $a1, $s3, $zero	#Address of input buffer
+	add $a2, $s5, $zero	#Number of characters to write
 	syscall
+
 #Zamknij plik
 	li $v0, 16
-	add $a0, $s4, $zero	#Skopiowanie wska�nika na plik
+	add $a0, $s4, $zero	#Copying file pointer
 	syscall
-#Zako�cz program
+
+#Finish programme
 	li $v0, 10
 	syscall
